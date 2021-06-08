57e4cb8e674ed54417c4e4d3eab12d7db17b8be2
asma <asma@1D-ASMA.DC-CAPES.COM>
2021-06-08 12:54:17 +0100

    first commit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Address() {
+    return ( <
+        div >
+        <
+        h3 > Adresse: Cité Mourouj kalaat andalous < /h3> < /
+        div >
+    );
+}
---
+++ src/Component/Profile/ProfilPhoto.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProfilPhoto = () => {
+    return ( <
+        div className = "Photo" >
+        <
+        img className = "Photo"
+        src = "./Asma.PNG"
+        alt = "profilphoto" / >
+        <
+        /div>
+    )
+};
+export default ProfilPhoto;
